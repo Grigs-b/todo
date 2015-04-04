1dcc1217bdcd47bf8d2579cf522dd296b8a62f27
Grigs-b <rgrigsby@inovasolutions.com>
2015-04-04 11:05:12 -0400

    Initial Commit

---
+++ todo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  todo
+//
+//  Created by Ryan Grigsby on 4/4/15.
+//  Copyright (c) 2015 Ryan Grigsby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
